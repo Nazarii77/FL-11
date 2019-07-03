cd2001cf689d88778a44bb54998a6ddbf52f7ffd
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-04 00:28:34 +0300

    task 1
    task 1

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -9,29 +9,13 @@ b1=parseInt(arr[2]);
 b2=parseInt(arr[3]);
 c1=parseInt(arr[4]);
 c2=parseInt(arr[5]);
- 	
-function check_middle (с,xa,xb) {
 
-	//console.log(с,xa,xb);
-c = parseInt(c);
-xa = parseInt(xa);
-xb = parseInt(xb);
-
-     alert(typeof c);
-     alert( (xa+xb)/2 );
-
-	if (c === (xa+xb)/2){
-		return true;
-	} else {
-		return false;
-	}
-}
-
-alert('check_middle(c1,a1,b1) = ' + check_middle(c1,a1,b1) );
-alert('check_middle(c2,a2,b2)= ' + check_middle(c2,a2,b2) );
-
-if (check_middle(c1,a1,b1) && check_middle(c2,a2,b2) ) {
-	console.log('true');
+if ( c1 ===(a1+b1)/2 ) {
+		if ( c2 ===(a2+b2)/2 ) {
+		console.log('true');
+		}else{
+		console.log('false');
+		}
 } else{
 	console.log('false');
 }
