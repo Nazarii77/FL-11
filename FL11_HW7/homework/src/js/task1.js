f992693ff33d09290cd3a1e09fc6b434de9df118
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-05 01:23:19 +0300

    Update task1.js
    task 1

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,31 +1,70 @@
-let input = prompt('Enter your email here');
-let password = ""; 
-let newpassword = ""; 
+var  input = '';
+let password = '';
+const newpassword = '';
 
-if (input === null){
-	alert('Canceled.');
-} else if (input.length < 6 ) {
-	alert('I don`t know any emails having name length less than 6 symbols');
-} else if (input === "user@gmail.com"){
-	 password = prompt('Enter your password');
-	 if (password==="UserPass") {
-	 	if(confirm("Do you want to change your password?")){
-	 		if ( password=== prompt('Enter your OLD password')) {
-	 				alert("1111");
-	 		} 
-	 	} else {
-	 		alert("You have failed the change.");
-	 	}
+input = prompt('Enter your email here');
 
-	 } else {
-	 	alert('Wrong password');
-	 }
+if (input === null) {
+    alert('Canceled.');
+} else if (input.length < 6) {
+    alert('I don`t know any emails having name length less than 6 symbols');
+} else if (input === "user@gmail.com") {
+    password = prompt('Enter your password');
+    if (password === 'UserPass') {
+        let sConfirmation = confirm('Do you want to change your password?');
+        if (sConfirmation) {
+            let olduserpass = prompt('Enter your OLD password');
+            if (password === olduserpass) {
+                let newpassword = prompt('Enter your NEW password');
+                if (newpassword.length < 5) {
+                    alert('It’s too short password. Sorry.');
+                } else {
+                    if (newpassword === prompt('REENTER your NEW password')) {
+                        alert('You have successfully changed your password.');
+                    } else {
+                        alert('You wrote the wrong password');
+                    }
+                }
 
-} else if (input === "admin@gmail.com"){
-	 password === prompt('Enter your password');
-	 if (password==="AdminPass") {
-	 			alert("222");
-	 } else {
-	 	alert('Wrong password');
-	 }
+            } else if (olduserpass === null) {
+                alert('Cancelled');
+            } else {
+                alert('Wrong password');
+            }
+        } else {
+            alert('You have failed the change.');
+        }
+    } else if (password === '') {
+        alert('Cancelled');
+    } else {
+        alert('Wrong password');
+    }
+} else if (input === 'admin@gmail.com') {
+    password = prompt('Enter your password');
+    if (password === "AdminPass") {
+        let sConfirmation = confirm('Do you want to change your password?');
+        if (sConfirmation) {
+            let olduserpass = prompt('Enter your OLD password');
+            if (password === olduserpass) {
+                let newpassword = prompt('Enter your NEW password');
+                if (newpassword.length < 5) {
+                    alert('It’s too short password. Sorry.');
+                } else {
+                    if (newpassword === prompt('REENTER your NEW password')) {
+                        alert('You have successfully changed your password.');
+                    } else {
+                        alert('You wrote the wrong password');
+                    }
+                }
+            } else if (olduserpass === null) {
+                alert('Cancelled');
+            }
+        } else {
+            alert('You have failed the change.');
+        }
+    } else {
+        alert('Wrong password');
+    }
+} else {
+	    alert('I don’t know you');
 }
