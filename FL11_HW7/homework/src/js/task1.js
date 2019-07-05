c32ce248a40739c136850a472902fbc4d09aac01
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-05 09:36:06 +0300

    rewritten variables

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,21 +1,23 @@
-var input = '';
-let password = '';
-const newpassword = '';
+let inputEmail = '';
+let inputpassword = '';
+let newpassword = '';
+let sConfirmation = '';
+let olduserpass = '';
 
-input = prompt('Enter your email here');
+inputEmail = prompt('Enter your email here');
 
-if (input === null) {
+if (inputEmail === null) {
     alert('Canceled.');
-} else if (input.length < 6) {
+} else if (inputEmail.length < 6) {
     alert('I don`t know any emails having name length less than 6 symbols');
-} else if (input === 'user@gmail.com') {
-    password = prompt('Enter your password');
-    if (password === 'UserPass') {
-        let sConfirmation = confirm('Do you want to change your password?');
+} else if (inputEmail === 'user@gmail.com') {
+    inputpassword = prompt('Enter your password');
+    if (inputpassword === 'UserPass') {
+        sConfirmation = confirm('Do you want to change your password?');
         if (sConfirmation) {
-            let olduserpass = prompt('Enter your OLD password');
-            if (password === olduserpass) {
-                let newpassword = prompt('Enter your NEW password');
+            olduserpass = prompt('Enter your OLD password');
+            if (inputpassword === olduserpass) {
+                newpassword = prompt('Enter your NEW password');
                 if (newpassword.length < 5) {
                     alert('It’s too short password. Sorry.');
                 } else {
@@ -34,19 +36,19 @@ if (input === null) {
         } else {
             alert('You have failed the change.');
         }
-    } else if (password === '') {
+    } else if (inputpassword === '') {
         alert('Cancelled');
     } else {
         alert('Wrong password');
     }
-} else if (input === 'admin@gmail.com') {
-    password = prompt('Enter your password');
-    if (password === 'AdminPass') {
-        let sConfirmation = confirm('Do you want to change your password?');
+} else if (inputEmail === 'admin@gmail.com') {
+    inputpassword = prompt('Enter your password');
+    if (inputpassword === 'AdminPass') {
+        sConfirmation = confirm('Do you want to change your password?');
         if (sConfirmation) {
-            let olduserpass = prompt('Enter your OLD password');
-            if (password === olduserpass) {
-                let newpassword = prompt('Enter your NEW password');
+            olduserpass = prompt('Enter your OLD password');
+            if (inputpassword === olduserpass) {
+                newpassword = prompt('Enter your NEW password');
                 if (newpassword.length < 5) {
                     alert('It’s too short password. Sorry.');
                 } else {
