327b9329f7a3975780054324935b245078d951bb
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-07 01:38:30 +0300

    task 2
    task 2

---
+++ FL11_HW7/homework/src/js/task2.js
@@ -0,0 +1,65 @@
+let sConfirmation = true;
+let wantstoplayagain = true;
+
+sConfirmation = confirm('Do you want to play a game?');
+while (wantstoplayagain) {
+    let random1 = 0;
+    let entered = '';
+    let prize = 0;
+    let alreadywon = false;
+    let wantstocontinue = true;
+    let lowrandom = 0;
+    let highrandom = 8;
+    let firstprize = 100;
+    let secondprize = 50;
+    let thirdprice = 25;
+    let prizemultiplier = 1;
+    let attemptsleft = 3;
+    let possibleprize = 0;
+    if (sConfirmation === null) {
+        alert('You did not become a billionaire, but you could.');
+    } else {
+        while (wantstocontinue) {
+            attemptsleft = 3;
+            for (var i = 0; i < 3; i++) {
+                if (alreadywon === false) {
+                    random1 = Math.floor(Math.random() * (highrandom - lowrandom + 1) + lowrandom);
+                    switch (i) {
+                        case 0:
+                            possibleprize = firstprize * prizemultiplier;
+                            break;
+                        case 1:
+                            possibleprize = secondprize * prizemultiplier;
+                            break;
+                        case 2:
+                            possibleprize = thirdprice * prizemultiplier;
+                            break;
+                        default:
+                            console.log("Out of range for possible");
+                            break;
+                    }
+
+                    entered = Number(prompt('Choose a roulette pockett number from ' + lowrandom + ' to ' + highrandom + '  \nAttempts left: ' + attemptsleft + ' \nTotal prize: ' + prize + '$ \nPossible prize on current attempt: ' + possibleprize + '$'));
+                    attemptsleft = attemptsleft - 1;
+                    if (random1 === entered) {
+                        prize = prize + possibleprize;
+                        alreadywon = true;
+                    }
+                }
+            }
+            if (alreadywon === false) {
+                alert('x');
+                wantstoplayagain = confirm('Do you want to play again?');
+                wantstocontinue = false;
+                sConfirmation = true;
+            } else {
+                wantstocontinue = confirm('Congratulation, you won!   Your prize is: ' + prize + ' $. Do you want to continue?');
+                if (wantstocontinue) {
+                    alreadywon = false;
+                    highrandom = highrandom + 4;
+                    prizemultiplier = prizemultiplier * 2;
+                }
+            }
+        }
+    }
+}
