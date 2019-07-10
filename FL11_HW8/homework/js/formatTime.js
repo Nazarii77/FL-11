7b901118327046462084367757282d6932667733
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-11 01:49:47 +0300

    format time

---
+++ FL11_HW8/homework/js/formatTime.js
@@ -0,0 +1,20 @@
+let formattedminutes = 0;
+let formatteddays = 0;
+let formattedhours = 0;
+
+function formatTime(minutes) {
+    formattedminutes = minutes % 60;
+    formattedhours = (minutes - formattedminutes) / 60;
+    if (formattedhours >= 24) {
+        formatteddays = parseInt(formattedhours / 24);
+        formattedhours = formattedhours - formatteddays * 24;
+    }
+
+    return formatteddays + ' day(s) ' + formattedhours + ' hour(s) ' + formattedminutes + ' minutes';
+}
+
+console.log(formatTime(11166));
+
+formatTime(120); //=> 0 day(s) 2 hour(s) 0 minute(s).
+formatTime(59); //=> 0 day(s) 0 hour(s) 59 minute(s).
+formatTime(3601); //=> 1 day(s) 0 hour(s) 1 minute(s).
