7b4573f223c5002b4d79935316991c0e0eadfbc0
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-12 00:55:08 +0300

    massive of functions fix
    massive of functions fix

--- FL11_HW8/homework/js/pipe.js
+++ FL11_HW8/homework/js/pipe.js
@@ -4,13 +4,14 @@ function addOne(x) {
     return x + 1;
 }
 
- function pipe(receivenum,receivefunc) {
+ function pipe(receivenum, ...args) {
     initnumber = receivenum;
-    for (var j = 1; j < arguments.length; j++) {
-        initnumber = receivefunc(initnumber);
+    for (var j = 0; j+1 < arguments.length; j++) {
+        initnumber = args[j](initnumber);
     }
     return initnumber;
  }
 
 pipe(1, addOne); //=> 2
 pipe(1, addOne, addOne); //=> 3
+pipe(1, addOne, addOne, addOne); //=> 4
