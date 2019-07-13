448ac36bc6fa32be66943370c5581028363d5293
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-13 15:19:04 +0300

    first task

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+function getNumbers(mystring) {
+    let numarr = [];
+    let strarr = mystring.split('');
+    for (let i = 0; i < strarr.length; i++) {
+        let element = parseInt(strarr[i]);
+        if (!isNaN(element)) {
+            numarr.push(element);
+        }
+    }
+    console.log(numarr);
+}
+
+getNumbers('string'); // returns [] 
+getNumbers('n1um3ber95'); // returns [1,3,9,5]
