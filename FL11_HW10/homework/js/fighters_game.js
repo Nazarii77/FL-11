b5b01f75640fd3cc9884892c3b0b71053ea79833
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-19 22:12:31 +0300

    fighters game perfection
    fighters game perfection

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,53 +1,89 @@
    function Fighter(input) {
-  let loses = 0;
-  let wins = 0;
-  let currenthp = 100;
-  let maxhp =100;
-  let minhp =0;
-        return {
-            getName: () => input.name,
-            getDamage: () => input.damage,
-            getAgility: () => input.agility,
-            getHealth: () => input.hp,
-            addWin: () => wins++,
-            addLoss: () => loses++,
-            heal:( healvalue) => {  
-              currenthp += healvalue;
-              if (currenthp >= maxhp) {
-                currenthp= maxhp;
-              }
-            }, 
-            dealDamage:(damagevalue) => {  
-              currenthp -= damagevalue;
-              if (currenthp <= minhp) {
-                currenthp = minhp;
-              }
-            }, 
-            logCombatHistory: () => console.log('Name: '+ input.name +', Wins: ' +wins+', Losses: '+ loses),
-            attack:(secondplayer) => {
-              let percentage = 100;
-              let agilepercentage = secondplayer.getAgility()/percentage;
-                if (Math.random() <= agilepercentage){
-                 console.log( input.name + ' makes ' + input.damage +' damage ' + 'to ' + secondplayer.getName() +'' ); 
-                } else {
-                  console.log( input.name + 'attack missed'); 
-                }
-            }
-         
-         }
-    }
-    
-  
-  
-const myFighter = new Fighter({name: 'John', damage: 20, hp: 100, agility: 25}); // returns an object with methods
- //console.log(myFighter);
+       let loses = 0;
+       let wins = 0;
+       let currenthp = input.hp;
+       let maxhp = 100;
+       let minhp = 0;
+       return {
+           getName: () => input.name,
+           getDamage: () => input.damage,
+           getAgility: () => input.agility,
+           getHealth: () => currenthp,
+           addWin: () => wins++,
+           addLoss: () => loses++,
+           heal: (healvalue) => {
+               currenthp += healvalue;
+               if (currenthp >= maxhp) {
+                   currenthp = maxhp;
+               }
+           },
+           dealDamage: (damagevalue) => {
+               currenthp -= damagevalue;
+               if (currenthp <= minhp) {
+                   currenthp = minhp;
+               }
+           },
+           logCombatHistory: () => {
+               console.log('Name: ' + input.name + ', Wins: ' + wins + ', Losses: ' + loses);
+           },
+           attack: (secondplayer) => {
+               let percentage = 100;
+               let agilepercentage = secondplayer.getAgility() / percentage;
+               if (Math.random() <= agilepercentage) {
+                   let damage = input.damage;
+                   console.log(input.name + ' makes ' + damage + ' damage ' + 'to ' + secondplayer.getName() + '');
+                   secondplayer.dealDamage(damage);
+               } else {
+                   console.log(input.name + ' attack missed');
+               }
+           }
 
-const myFighter2 = new Fighter({name: 'Peter', damage: 40, hp: 100, agility: 35}); // returns an object with methods
+       }
+   }
 
-//let name = myFighter.name;
-//console.log(name); // undefined
+   function battle(fighter1, fighter2) {
+       let health1 = fighter1.getHealth();
+       let health2 = fighter2.getHealth();
+       let informdeath = ' is dead and can`t fight';
+       if (health1 === 0) {
+           console.log(fighter1.getName() + informdeath);
+       } else if (health2 === 0) {
+           console.log(fighter2.getName() + informdeath);
+       } else {
+           while (health1 !== 0 && health2 !== 0) {
+               fighter1.attack(fighter2);
+               health2 = fighter2.getHealth();
+               if (health2 > 0) {
+                   //dead can not attack 
+                   fighter2.attack(fighter1);
+                   health1 = fighter1.getHealth();
+               }
+               //console.log(health2 +"    "+ health1);
+           }
 
-myFighter.attack(myFighter2);
-// John make 20 damage to Jack
-myFighter2.attack(myFighter);
-// Jack attack missed
+           if (health1 === 0) {
+               fighter2.addWin();
+               fighter1.addLoss();
+           } else {
+               fighter1.addWin();
+               fighter2.addLoss();
+           }
+       }
+
+   }
+
+   const myFighter = new Fighter({
+       name: 'John',
+       damage: 20,
+       hp: 100,
+       agility: 25
+   });
+
+   const myFighter2 = new Fighter({
+       name: 'Peter',
+       damage: 40,
+       hp: 100,
+       agility: 35
+   });
+
+   battle(myFighter, myFighter2)
