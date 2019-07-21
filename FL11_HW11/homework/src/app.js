8d12ecab44ea2ba4b90966509f72de7747109558
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-07-21 23:20:31 +0300

    max quantity check

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,6 +1,9 @@
 let rootNode = document.getElementById('root');
-document.getElementById('addBtn').disabled = true;
+let disableEdit = false;
+let max_item = 10;
+let current_item =0;
 
+document.getElementById('addBtn').disabled = true;
 document.getElementById('todoInput').addEventListener('keyup', function() {
     let nameInput = document.getElementById('todoInput').value;
     if (nameInput !== '') {
@@ -11,52 +14,59 @@ document.getElementById('todoInput').addEventListener('keyup', function() {
 });
 
  function addNewToDO() {
-  let li = document.createElement('li');
-  let label = document.createElement('label');
-  let inputValue = document.getElementById('todoInput').value;
-  let t = document.createTextNode(inputValue);
+  current_item++;
+  if(checkitemquantity(current_item,max_item)){ 
+  
+    let li = document.createElement('li');
+    let label = document.createElement('label');
+    label.setAttribute('class', 'label-class');
+    let inputValue = document.getElementById('todoInput').value;
+    let t = document.createTextNode(inputValue);
 
-  let icheck = document.createElement('i');
-  icheck.setAttribute('class', 'material-icons check');
-  //icheck.setAttribute('onclick', 'editThis(this)');
+    let icheck = document.createElement('i');
+    icheck.setAttribute('class', 'material-icons check');
+    //icheck.setAttribute('onclick', 'editThis(this)');
+     
+    let incheckbox = document.createElement('input');
+    incheckbox.setAttribute('class', 'checkmark');
+    incheckbox.setAttribute('onclick', 'replaceCheckBox(this)');
+    incheckbox.setAttribute('type', 'checkbox');
+    icheck.appendChild(incheckbox);
+    li.appendChild(icheck);
    
-  let incheckbox = document.createElement('input');
-  incheckbox.setAttribute('class', 'checkmark');
-  incheckbox.setAttribute('onclick', 'replaceCheckBox(this)');
-  incheckbox.setAttribute('type', 'checkbox');
-  icheck.appendChild(incheckbox);
-  li.appendChild(icheck);
- 
-  //main
-  label.appendChild(t);
-  li.appendChild(label);
-  //edit
-  let iedit = document.createElement('i');
-  iedit.setAttribute('class', 'material-icons check edit');
-  iedit.setAttribute('onclick', 'editThis(this)');
-  let t2 = document.createTextNode('edit');
-  iedit.appendChild(t2); 
-  li.appendChild(iedit);
-  //delete
-  let i = document.createElement('i');
-  i.setAttribute('class', 'material-icons check delete');
-  i.setAttribute('onclick', 'deleteParent(this)');
-  let t3 = document.createTextNode('delete');
-  i.appendChild(t3); 
-  li.appendChild(i);
+    //main
+    label.appendChild(t);
+    li.appendChild(label);
+    //edit
+    let iedit = document.createElement('i');
+    iedit.setAttribute('class', 'material-icons check edit');
+    iedit.setAttribute('onclick', 'editThis(this)');
+    let t2 = document.createTextNode('edit');
+    iedit.appendChild(t2); 
+    li.appendChild(iedit);
+    //delete
+    let i = document.createElement('i');
+    i.setAttribute('class', 'material-icons check delete');
+    i.setAttribute('onclick', 'deleteParent(this)');
+    let t3 = document.createTextNode('delete');
+    i.appendChild(t3); 
+    li.appendChild(i);
 
-  if (inputValue !== '') {
-    document.getElementById('mylist').appendChild(li);
-  }
-  document.getElementById('todoInput').value = '';
-  document.getElementById('addBtn').setAttribute('disabled', null);
- 
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = 'none';
+    if (inputValue !== '') {
+      document.getElementById('mylist').appendChild(li);
     }
-  }
+    document.getElementById('todoInput').value = '';
+    document.getElementById('addBtn').setAttribute('disabled', null);
+   
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = 'none';
+      }
+    }
+  } else {
+    current_item--;
+  }  
 }
 
 
@@ -69,36 +79,58 @@ function replaceCheckBox(e) {
  
   function deleteParent(e) {
     e.parentNode.parentNode.removeChild(e.parentNode);
+    current_item--;
+    let nameInput = document.getElementById('todoInput').value;
+    if (nameInput !== '') {
+          document.getElementById('addBtn').disabled = false;
+    }
+    document.getElementById('maxwarning').style.display = 'none';
+    document.getElementById('todoInput').removeAttribute('disabled');
 }
 
 function editThis(e){
- 
-    let li = e.parentElement;
-    let labeltext = li.getElementsByTagName('label')[0].textContent;
-    let p = document.createElement('p');
-    let i = document.createElement('i');
-    i.setAttribute('class', 'material-icons check save');
-    i.setAttribute('onclick', 'saveAndDelete(this)');
-    let t = document.createTextNode('save');
-    i.appendChild(t);
-    //<i class="material-icons check save" onclick="saveAndDelete(this)" >save</i>
-    
-     let input = document.createElement('input');
-     //input.setAttribute('placeholder', labeltext);
-     input.value = labeltext;
-     input.setAttribute('class', 'editinput');
-    //<input type="text"   placeholder="Add new action" class="editinput">
-    p.appendChild(input);
-    p.appendChild(i);
-    li.appendChild(p);
+    if (!disableEdit) {
+      let li = e.parentElement;
+      let labeltext = li.getElementsByTagName('label')[0].textContent;
+      let p = document.createElement('p');
+      let i = document.createElement('i');
+      i.setAttribute('class', 'material-icons check save');
+      i.setAttribute('onclick', 'saveAndDelete(this)');
+      let t = document.createTextNode('save');
+      i.appendChild(t);
+      let input = document.createElement('input');
+      input.value = labeltext;
+      input.setAttribute('class', 'editinput');
+      p.appendChild(input);
+      p.appendChild(i);
+      li.appendChild(p);  
+      disableEdit = true;
+    } 
 }
 
 function saveAndDelete(e){
-
+    disableEdit = false;
     let li = e.parentElement.parentElement;
     let labeltext= li.getElementsByTagName('label')[0];
     let replacetext = li.getElementsByTagName('p')[0].getElementsByTagName('input')[0].value; 
      labeltext.innerHTML = replacetext;
     deleteParent(e);
+    current_item++;
+    document.getElementById('maxwarning').style.display = 'block';
+    document.getElementById('todoInput').setAttribute('disabled', null);
+    document.getElementById('addBtn').setAttribute('disabled', null);
+}
+
+
+function checkitemquantity(current_item,max_item){
+  if (current_item > max_item){
+      document.getElementById('addBtn').disabled = true;
+      document.getElementById('maxwarning').style.display = 'block';
+      document.getElementById('todoInput').setAttribute('disabled', null);
+
+      return false;
+  } else {
+    return true;
+  }
 
 }
