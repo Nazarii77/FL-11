0e29292abc1d7ea3ec769aea4684dab1cb325c98
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-08-18 14:39:17 +0300

    no need accessors
    no need accessors. changed function polyfill

--- FL_11_16_homework_inheritance/homework/js/task1.js
+++ FL_11_16_homework_inheritance/homework/js/task1.js
@@ -1,28 +1,33 @@
-  function assign(target, ...sources) {
-      sources.forEach(source => {
-          let descriptors = Object.keys(source).reduce((descriptors, key) => {
-              descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
-              return descriptors;
-          }, {});
-          // by default, Object.assign copies enumerable Symbols too
-          Object.getOwnPropertySymbols(source).forEach(sym => {
-              let descriptor = Object.getOwnPropertyDescriptor(source, sym);
-              if (descriptor.enumerable) {
-                  descriptors[sym] = descriptor;
-              }
-          });
-          Object.defineProperties(target, descriptors);
-      });
-      return target;
-  }
+function assign(target, varArgs) {
+    'use strict';
+    //handling incorrect target
+    if (target === null || target === undefined) {
+        throw new TypeError('Cannot convert undefined or null to object');
+    }
 
+    var to = Object(target);
 
-  const defaults = {
-      a: 123,
-      b: 777
-  };
-  const options = {
-      a: 456
-  };
-  const configs = assign({}, defaults, options); // => {a: 456, b: 777}
-  console.log(configs);
+    for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource !== null && nextSource !== undefined) {
+            for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                }
+            }
+        }
+    }
+    return to;
+}
+
+const defaults = {
+    a: 123,
+    b: 777
+};
+const options = {
+    a: 456
+};
+const configs = assign({}, defaults, options); // => {a: 456, b: 777}
+console.log(configs);
