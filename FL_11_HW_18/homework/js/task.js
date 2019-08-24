3f7cf8fee178978cd736b00ec188f519ff2e294b
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-08-25 00:25:31 +0300

    displaying userlist

--- FL_11_HW_18/homework/js/task.js
+++ FL_11_HW_18/homework/js/task.js
@@ -1,4 +1,3 @@
- 
 
 
 sortArrayOfObjects = (arr, key) => {
@@ -17,22 +16,73 @@ function getNames(obj) {
     var arr = [];
     for (var i = 0; i < obj.length; i++) {
         arr.push(obj[i].name);
+        addUser(obj[i].name);
     }
     return arr;
 }
  
 
 async function fetchJsonAsync(url) {
-    try {
+     try {
         const request = await fetch(url);
         const text = await request.text(); {
             var obj = JSON.parse(text);
+            //console.log(Object.values(obj)); 
+
             var sKey = 'name';
             obj = sortArrayOfObjects(obj, sKey);
             return getNames(obj);
         }
-    } catch (error) {
-        console.log(`ERROR: ${error.stack}`);
-    }
+      } catch (error) {
+          console.log(`ERROR: ${error.stack}`);
+      }
 }
-fetchJsonAsync('https://jsonplaceholder.typicode.com/users').then(res => console.log(res))
+
+
+fetchJsonAsync('https://jsonplaceholder.typicode.com/users').then(res => console.log(res));
+
+
+
+const rootNode = document.getElementById('root');
+
+let maindiv = document.createElement('div');
+maindiv.setAttribute('id', 'maindiv');
+rootNode.appendChild(maindiv);
+let divaddresult = document.createElement('div');
+divaddresult.setAttribute('class', 'centered');
+let ul = document.createElement('ul');
+ul.setAttribute('id', 'mylist');
+divaddresult.appendChild(ul);
+maindiv.appendChild(divaddresult);
+
+ 
+
+function addUser(name) {
+		let li = document.createElement('li');
+		li.setAttribute('class', 'li-uncheck');
+		let label = document.createElement('label');
+		label.setAttribute('class', 'label-class');
+		label.setAttribute('onclick', 'editlabel(this)');
+		//label.setAttribute('for', item.id);
+		//label.setAttribute('id', item.id);
+		let textrecovered = document.createTextNode(name);
+		label.appendChild(textrecovered);
+
+
+
+		let deletecheckbox = document.createElement('img');
+		deletecheckbox.setAttribute('src', 'assets/img/remove-s.jpg');
+		deletecheckbox.setAttribute('class', 'deleteimg');
+		deletecheckbox.setAttribute('onclick', 'deleteToDO(this)');
+		label.appendChild(deletecheckbox);
+
+		let incheckbox = document.createElement('input');
+		incheckbox.setAttribute('class', 'checkmark');
+		incheckbox.setAttribute('type', 'checkbox');
+		li.appendChild(incheckbox);
+		li.appendChild(label);
+		ul.appendChild(li);
+
+}
+
+ 
