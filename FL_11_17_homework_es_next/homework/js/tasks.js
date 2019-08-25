c448e1906d1c384051abd288ec6f4318d4846cf6
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-08-25 12:35:49 +0300

    removed debug messages
    removed debug messages

--- FL_11_17_homework_es_next/homework/js/tasks.js
+++ FL_11_17_homework_es_next/homework/js/tasks.js
@@ -4,20 +4,10 @@ function maxElement(arrayToProcess) {
 
 const array = [1, 2, 3, 4, 56, 7, 8, 76, 5, 241, 5, 356, 567, 2];
 
-console.log(maxElement(array));
-
-
-
 
 function copyArray(arrayToProcess) {
     return [...arrayToProcess];
 }
-
-const array2 = [1, 2, 3];
-const copiedArray = copyArray(array2);
-console.log(array2, copiedArray);
-console.log(array2 === copiedArray);
-
 
 
 function addUniqueId(obj) {
@@ -34,11 +24,6 @@ const target1 = {
 const target2 = {
     name: 123
 };
-
-console.log(addUniqueId(target2).id === addUniqueId(target1).id);
-
-
-
 
 function regroupObject(obj) {
     let newObj = {
@@ -62,21 +47,11 @@ const oldObj = {
     }
 };
 
-console.log(regroupObject(oldObj));
-
-
-
 
 function findUniqueElements(array) {
     let unique = [...new Set(array)];
     return unique;
 }
-
-const array = [1, 1, 23, 3, 4, 5, 6, 5, 4, 23, 2, 1, 1, 1, 1, 1]
-
-console.log(findUniqueElements(array));
-
-
 
 
 function hideNumber(phonenumber) {
@@ -86,12 +61,6 @@ function hideNumber(phonenumber) {
 
 }
 
-const phonenumber = '0123456789'
-
-console.log(hideNumber(phonenumber));
-
-
-
 
 function throwErrorIfMissing() {
     throw new Error('Missing property');
@@ -100,11 +69,6 @@ function throwErrorIfMissing() {
 function add(a = throwErrorIfMissing(), b = throwErrorIfMissing()) {
     return a + b;
 }
-
-console.log(add(1, 3));
-
-console.log(add(1));
-
 
 
 sortArrayOfObjects = (arr, key) => {
@@ -139,8 +103,6 @@ function fetchJson(url) {
         })
         .catch(error => console.log(`ERROR: ${error.stack}`));
 }
-fetchJson('https://jsonplaceholder.typicode.com/users').then(res => console.log(res))
-
 
 
 async function fetchJsonAsync(url) {
@@ -156,4 +118,3 @@ async function fetchJsonAsync(url) {
         console.log(`ERROR: ${error.stack}`);
     }
 }
-fetchJsonAsync('https://jsonplaceholder.typicode.com/users').then(res => console.log(res))
