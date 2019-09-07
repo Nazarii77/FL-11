cde0af62f627f585d1c52d9f0d85ce5f3dba3d33
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-09-07 15:15:58 +0300

    init

--- FL11_HW21/myNews/src/app/app.module.ts
+++ FL11_HW21/myNews/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
