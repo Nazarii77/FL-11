16d63fdd8486109ea9da2f6770255db1cabeba12
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-09-07 19:46:57 +0300

    tests

--- FL11_HW21/myNews/src/app/news/news.component.ts
+++ FL11_HW21/myNews/src/app/news/news.component.ts
@@ -12,11 +12,16 @@ export interface News {
 export class NewsComponent implements OnInit {
   public news: News[] = [{
     title: 'War in Ukraine',
-    description:'The Russian military intervention in Ukraine, sometimes called the Russo-Ukrainian War,[68][69][70] is a series of military actions that started in February 2014 and continues into 2019, including in the Crimean peninsula, the Donbas region in eastern Ukraine, and related activities in other locations.\n' +
+    description:'The Russian military intervention in Ukraine, sometimes called the Russo-Ukrainian War,  is a series of military actions that started in February 2014 and continues into 2019, including in the Crimean peninsula, the Donbas region in eastern Ukraine, and related activities in other locations.\n' +
       '\n' +
       'After Euromaidan protests and the fall of Ukrainian president Viktor Yanukovych, Russian soldiers without insignias took control of strategic positions and infrastructure within the Ukrainian territory of Crimea.'
 
-  }]
+  },
+    {
+      title: 'War in Syria',
+      description: 'is an ongoing multi-sided civil war in Syria fought between the Ba\'athist Syrian Arab Republic led by President Bashar al-Assad, along with domestic and foreign allies, and various domestic and foreign forces opposing both the Syrian government and each other in varying combinations.'
+    }
+]
   constructor() { }
 
   ngOnInit() {
