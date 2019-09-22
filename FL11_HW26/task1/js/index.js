00bda2b53915ff2e2645c754a1f0b740fcf08b23
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-09-23 00:34:32 +0300

    first task

--- FL11_HW26/task1/js/index.js
+++ FL11_HW26/task1/js/index.js
@@ -7,7 +7,7 @@ $('#title').css({"align" : "center", "margin-top" : "0px" });
 // div after header: add double border and padding 20px
 $('#header').next().css({"padding" : "20px", "border" : "double"});
 // change color for all h2 and set top margin to 0
-$("h2").css({"color" : "green" ,"margin-top" : "0px"});
+$("h2").css({"color" : "purple" ,"margin-top" : "0px"});
 // set font size 18px for all .list direct child
 $(".list > *").css({"font-size" : "18px"});
 
@@ -32,15 +32,15 @@ $('#list-1 > li:empty').css({"display" : "none"});
   // for all even li set margin-left -20px
 $('#list-3 > li:even').css({"margin-left" : "-20px"});
   // for the first li set any different color
-$('#list-3 > li:first').css({"color" : "red"});
+$('#list-3 > li:first').css({"color" : "brown"});
   // for all li with index > 5 set color to #ccc
-
+$('#list-3 > *:gt(5)').css({"color" : "#ccc"});
 // show ul which is parent
-
+$('ul:parent').css({"display" : "block"});
 // for li wich has em add red color
-
+$('li:has(em)').css({"color" : "red"});
 // for li which contains text Buratino set font weight to bold
-
+$('li:contains(Buratino)').css({"font-weight" : "bold"});
 
 
 
@@ -51,9 +51,9 @@ $('#list-3 > li:first').css({"color" : "red"});
 // ******************************************************
 
 // for b in p which is the only child set font size 36px
-
+$("p > b:only-child").css({"font-size" : "36px"});
 // for em in p which is the last child of type set color to green
-
+$("p > em:last-of-type").css({"color" : "green"});
 
 
 
@@ -64,11 +64,14 @@ $('#list-3 > li:first').css({"color" : "red"});
 // ******************************************************
 
 // set width 80px for input with attribute name ended by 'age'
+$("[name$='age']").css({"width" : "80px"});
 // set width 120px for input with attribute name started by 'my'
+$("[name^='my']").css({"width" : "120px"});
 // console.log checked checkbox
+console.log($("[name='checked_checkbox']"));
 // show all images with a cat
-
-
+$("[alt*='cat']").css({"display" : "block"});
+//$("[src*='cat']").css({"display" : "block"});
 // ******************************************************
 // is(), not(), has()
 // eq(), first(), last()
@@ -76,4 +79,7 @@ $('#list-3 > li:first').css({"color" : "red"});
 // children(), prev(), next(), siblings()
 // ******************************************************
 // for .mbox with index 3 set padding-top 50px
+$('.mbox:eq(3)').css({"padding-top" : "50px"});
 // for first div wraper for img set float left and border red
+$("[src*='img']").closest('div').first().css({"float" : "left", "border"  : "solid  5px red"});
+
