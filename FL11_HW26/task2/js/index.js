dfd420663b3309b36877c03608b1ec9d6eb4299c
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-09-24 17:42:44 +0300

    simple but working unfinished

--- FL11_HW26/task2/js/index.js
+++ FL11_HW26/task2/js/index.js
@@ -13,16 +13,34 @@ const $list = $(".list");
 const $input = $("#add-input");
 const $add = $("#add-submit");
 
-
-$add.click(function(){
-  //alert("Add was clicked.");
-    this.todos +=  [
-    {
-      text: "Buy milk",
-      done: false
-    }
-
-];
-
+$add.on( "click",function(){
+        var node = document.createTextNode($input.val());
+        let li = document.createElement('li');
+        li.setAttribute('class', 'item');
+        let spn = document.createElement('span');
+        spn.setAttribute('class', 'item-text');
+        spn.appendChild(node)
+        let btn = document.createElement('button');
+        btn.setAttribute('class', 'item-remove');
+        btn.textContent = 'Remove';
+        li.appendChild(spn);
+        li.appendChild(btn);
+        $input.val('') ;
+        $list.append(li)
+        this.todos +=  [
+            {   text: "Buy milk2",
+                done: false
+            }
+        ];
 });
-console.log(todos);
+
+$list.on("click", "button.item-remove", function(){
+    this.parentNode.parentNode.removeChild(this.parentNode);
+});
+
+$list.on("click", "span.item-text", function(event){
+   // console.log(this)    /// .parentNode.addClass("done");
+    $target = $(event.target);
+    $target.addClass("done");
+});
+
