c14045693f19f1e1e0470b2a57f911b701d85bb9
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-09-24 22:08:50 +0300

    MODULE PATTERN

--- FL11_HW26/task2/js/index.js
+++ FL11_HW26/task2/js/index.js
@@ -1,36 +1,64 @@
-const $list = $(".list");
-const $input = $("#add-input");
-const $add = $("#add-submit");
+let myToDoApp = (function() {
+    const $list = $(".list");
+    let btnCtrls = {};
 
+    // learning how to make plugin
+    $.fn.pluginDone = function () {
+        this.addClass("done");
+    };
 
-$add.on( "click",function adding(){
-    var node = document.createTextNode($input.val());
-    let li = document.createElement('li');
-    li.setAttribute('class', 'item');
-    let spn = document.createElement('span');
-    spn.setAttribute('class', 'item-text');
-    spn.appendChild(node)
-    let btn = document.createElement('button');
-    btn.setAttribute('class', 'item-remove');
-    btn.textContent = 'Remove';
-    li.appendChild(spn);
-    li.appendChild(btn);
-    $input.val('') ;
-    $list.append(li);
+    let pluginDoneUsage = function (event) {
+        $target = $(event.target);
+        $target.pluginDone();  //this is plugin usage
+    }
+
+    let deleting = function () {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    }
+
+    let addToDoClick = function(event) {
+        let $input = $("#add-input");
+        if ($input.val() == '') {$input.val('lazy todo')}
+        let node = document.createTextNode($input.val());
+        let li = document.createElement('li');
+        li.setAttribute('class', 'item');
+        let spn = document.createElement('span');
+        spn.setAttribute('class', 'item-text');
+        spn.appendChild(node)
+        let btn = document.createElement('button');
+        btn.setAttribute('class', 'item-remove');
+        btn.textContent = 'Remove';
+        li.appendChild(spn);
+        li.appendChild(btn);
+        $input.val('');
+        $list.append(li);
+    };
+
+    let bindBtnControls = function () {
+        var self = {};
+        self.myadd =  $("#add-submit");
+        return self;
+    };
+
+    let bindFunctions = function() {
+        // you can use jQuery
+        //$("#add-submit").on("click", addToDoClick)
+        $list.on("click", "button.item-remove", deleting);
+        $list.on("click", "span.item-text", pluginDoneUsage);
+        // but we have the controls object to use, so instead
+        btnCtrls.myadd.on('click', addToDoClick );
+    };
+
+    let init = function () {
+        btnCtrls = bindBtnControls();
+        bindFunctions();
+    };
+    return {
+        init: init
+    };
+})();
+
+//usage
+$("document").ready(function () {
+    myToDoApp.init();
 });
-
-$list.on("click", "button.item-remove", function(){
-    this.parentNode.parentNode.removeChild(this.parentNode);
-});
-
-//plugin learning how to make
-$.fn.done  = function() {
-    this.addClass("done");
-};
-
-$list.on("click", "span.item-text", function(event){
-    $target = $(event.target);
-    $target.done();  //this is plugin usage
-});
-
-
