72fa0834739c6942ffc36d589ad6eb0160c73436
Nazarii Boichyshyn <boichyshyn@gmail.com>
2019-12-02 21:05:30 +0200

    counter

---
+++ Code asked/closure.js
@@ -0,0 +1,43 @@
+function Counter(){
+
+	var count = 0;
+	return function() {
+		count++;
+		return count;
+	}
+}
+
+
+var counter = Counter();
+counter()
+
+console.log(counter());
+
+
+var counter2 = Counter();
+counter2()
+counter2()
+counter2()
+console.log(counter2());
+////////////////////////////////////////////////////////////////////////////// es15
+
+
+function Counter(){
+	var count = 0;
+	return () => count++
+}
+
+
+var counter = Counter();
+counter()
+counter()
+console.log(counter());
+
+
+var counter2 = Counter();
+counter2()
+counter2()
+counter2()
+console.log(counter2());
+
+///////////////////////////////////////////////////////////////////////////////////////////
